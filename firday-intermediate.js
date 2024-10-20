382c3465a04042a7477c74cc24687353cbdbb30a
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2024-10-20 10:45:05 +0200

    friday exercise

---
+++ firday-intermediate.js
@@ -0,0 +1,28 @@
+//Fibonacci Sequence
+
+// dedine a function takes numbers as parameters
+//define variables, and set one variable to be 1
+//for loop start from 1 and end at the givin number
+//shift the value of the variable
+//return the result
+
+//t-diagram
+//step: i=2, i=3
+//seq2:0,0,1
+//seq1:0,1,1
+//seq:-,seq=0+1=1,seq=1+1=2
+//output: 3
+
+function Fibonacci(num) {
+  let seq = 1;
+  let seq1 = 0;
+  let seq2 = 0;
+  for (let i = 2; i <= num; i++) {
+    seq2 = seq1;
+    seq1 = seq;
+    seq = seq1 + seq2;
+  }
+  return seq;
+}
+
+console.log(Fibonacci(4));
